48a2482cd8bf5f5c85bdb61ddff5a91184588d6d
Jeffrey Maas <jmaasy@gmail.com>
2022-12-06 00:37:18 +0100

    renamed actions

--- client/src/common/Analytics.tsx
+++ client/src/common/Analytics.tsx
@@ -6,39 +6,39 @@ const sendEvent = (args: UaEventOptions) => ReactGA.event(args);
 
 export const Analytics = {
     trackVotesReveal: (lobbyId: string) => {
-        const args = {category: "VotesReveal", action: "Revealing Votes", label: `RevealingVotes${lobbyId}`};
+        const args = {category: "VotesReveal", action: "revealing_votes", label: `RevealingVotes${lobbyId}`};
         sendEvent(args);
     },
     trackVoting: (vote: number | string) => {
-        const args = {category: "VoteUpdate", action: "Updating Vote", label: `VoteNumber${vote}`};
+        const args = {category: "VoteUpdate", action: "updating_vote", label: vote.toString()};
         sendEvent(args);
     },
     trackRegistering: () => {
-        const args = {category: "UserRegistered", action: "User Registered", label: `User Registered`};
+        const args = {category: "UserRegistered", action: "user_registered", label: `User Registered`};
         sendEvent(args);
     },
     trackCreatedLobby: () => {
-        const args = {category: "UserCreatedLobby", action: "User Created A Lobby", label: `User Created A Lobby`};
+        const args = {category: "UserCreatedLobby", action: "user_created_lobby", label: `User Created A Lobby`};
         sendEvent(args);
     },
     trackJoinedLobby: () => {
-        const args = {category: "UserJoinedLobby", action: "User Joined A Lobby", label: `User Joined A Lobby`};
+        const args = {category: "UserJoinedLobby", action: "user_joined_lobby", label: `User Joined A Lobby`};
         sendEvent(args);
     },
     trackLeftLobby: () => {
-        const args = {category: "UserLeftLobby", action: "User Left A Lobby", label: `User Left A Lobby`};
+        const args = {category: "UserLeftLobby", action: "user_left_lobby", label: `User Left A Lobby`};
         sendEvent(args);
     },
     trackConnectionLost: () => {
-        const args = {category: "ConnectionLost", action: "Connection Lost", label: `Connection to server was lost`};
+        const args = {category: "ConnectionLost", action: "connection_lost", label: `Connection to server was lost`};
         sendEvent(args);
     },
     trackConnectionSuccess: () => {
-        const args = {category: "ConnectionSuccess", action: "Connection Success", label: `Connection to server was successful`};
+        const args = {category: "ConnectionSuccess", action: "connection_success", label: `Connection to server was successful`};
         sendEvent(args);
     },
     trackConnectionRegained: () => {
-        const args = {category: "ConnectionRegained", action: "Connection Regained", label: `Connection to server has restored`};
+        const args = {category: "ConnectionRegained", action: "connection_regained", label: `Connection to server has restored`};
         sendEvent(args);
     },
     initializeTracking: () => {
